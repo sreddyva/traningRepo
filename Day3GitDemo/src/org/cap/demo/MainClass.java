96ecc064ada5990b4a055b5669a471defbfd3615
Training <Training@DIN39000545.corp.capgemini.com>
2016-07-07 10:03:04 +0530

    initial commit

---
+++ Day3GitDemo/src/org/cap/demo/MainClass.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		ProductDisplay productDisplay = new ProductDisplay();
+		productDisplay.productDisplayMethod();
+
+	}
+
+}
---
+++ Day3GitDemo/src/org/cap/demo/ProductDisplay.java
@@ -0,0 +1,7 @@
+package org.cap.demo;
+
+public class ProductDisplay {
+	public void productDisplayMethod(){
+		System.out.println("product!!!!!!!!!!!!!!!!");
+	}
+}
