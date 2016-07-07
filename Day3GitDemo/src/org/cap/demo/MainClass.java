d0310d8af18cd941925414a252335be350357621
Training <Training@DIN39000545.corp.capgemini.com>
2016-07-07 10:13:36 +0530

    added comments

--- Day3GitDemo/src/org/cap/demo/MainClass.java
+++ Day3GitDemo/src/org/cap/demo/MainClass.java
@@ -3,9 +3,10 @@ package org.cap.demo;
 public class MainClass {
 
 	public static void main(String[] args) {
+		System.out.println("Start??????????????");
 		ProductDisplay productDisplay = new ProductDisplay();
 		productDisplay.productDisplayMethod();
-
+		System.out.println("end!!!!!!!!!");
 	}
     
 }
